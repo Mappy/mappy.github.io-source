8aeeefc4f86bf8f35eddf64fd5ede706b0d4ca47
Ludovic Delauné <ludotux@gmail.com>
2013-12-20 16:27:16 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,11 @@ BRANCH=master
 TARGET_REPO=Mappy/mappy.github.io.git
 PELICAN_OUTPUT_FOLDER=output
 
-echo env
-
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
     if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "travis@travis-ci.org"
-        git config --global user.name "Travis"
+        git config --global user.email "ludovic.delaune@mappy.com"
+        git config --global user.name "ldgeo"
     fi
     #using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
