7e339f6b600eaeb71ecb53e628706b7f8361c076
Ludovic Delauné <ludovic.delaune@mappy.com>
2013-12-20 16:39:41 +0100

    change travis-ci token

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ PELICAN_OUTPUT_FOLDER=output
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
     if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "ludovic.delaune@mappy.com"
-        git config --global user.name "ldgeo"
+        git config --global user.email "travis@Travis-ci.org"
+        git config --global user.name "Travis"
     fi
-    #using token clone gh-pages branch
+
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd built_website
